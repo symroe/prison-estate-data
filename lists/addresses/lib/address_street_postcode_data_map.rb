ab3c0a8579b703f266664a3bc0fdb815a7ad3d1a
robmckinnon <rob@movingflow>
2017-02-10 12:10:49 +0000

    Add longer prison name to address maps file

--- lists/addresses/lib/address_street_postcode_data_map.rb
+++ lists/addresses/lib/address_street_postcode_data_map.rb
@@ -8,15 +8,18 @@ codes = LoadData.laa_codes ; nil
 prisons = LoadData.prison_finder_prisons ; nil
 former_prisons = LoadData.former_prisons ; nil
 addresses = LoadData.addresses ; nil
+official_names = LoadData.prison_map_prisons ; nil
 
-puts (%w[prison prison-name address] + addresses.first.morph_attributes.keys).join("\t")
+puts (%w[prison prison-name prison-short-name address] + addresses.first.morph_attributes.keys).join("\t")
 prisons.each do |prison|
   name = prison.prison
   address = Matcher.address_uprn(name, prison, addresses)
   code = Matcher.match_code(name, codes)
+  official_name = Matcher.match_official_name(name, official_names)
   short_name = Matcher.short_name(name)
   puts ([
     code,
+    official_name,
     short_name,
     address
   ] + addresses.detect{|a| a.key == address}.morph_attributes.values ).join("\t") if address
@@ -24,9 +27,11 @@ end
 former_prisons.each do |prison|
   address = Matcher.address_uprn(prison.name, prison, addresses)
   code = prison.prison
+  official_name = Matcher.match_official_name(prison.name, official_names)
   short_name = Matcher.short_name(prison.name)
   puts ([
     code,
+    official_name,
     short_name,
     address
   ] + addresses.detect{|a| a.key == address}.morph_attributes.values ).join("\t") if address
